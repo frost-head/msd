3721ead8738ba56c6ec846116b8d14e372d5635b
frost-head <ed.ayush2003@gmail.com>
2025-07-21 01:17:53 +0530

    basic script

--- msd.sh
+++ msd.sh
@@ -3,8 +3,33 @@
 mkdir -p /tmp/msd/
 
 
+
+calculate(){
+  pwd=$(pwd)
+  hostname=$(hostname)
+  whoami=$(whoami)
+  $cmd > /tmp/msd/tmp_out.txt
+}
+
+formatAndSaveFile(){
+  echo "==============[$title]====================" >> $1
+  echo "$whoami@$hostname at $pwd" >> $1
+  echo "" >> $1
+  echo "Command : $cmd" >> $1
+  echo "" >> $1
+  echo "$(cat /tmp/msd/tmp_out.txt)"
+  echo "Output : $(cat /tmp/msd/tmp_out.txt)" >> $1
+  echo "========================================" >> $1
+
+  echo "" >> $1
+}
+
+
 while [[ "$#" -gt 0 ]]; do
   case $1 in
+    -c|--copy) mode="copy"; shift ;;
+    -cd|--copyDoc) mode="copyDoc"; shift ;;
+    -l|--last) last=1; shift ;;
     -f|--filename) filename="$2"; shift ;;
     -t|--title) title="$2"; shift ;;
     *) echo "Unknown parameter passed: $1" ;;
@@ -12,26 +37,32 @@ while [[ "$#" -gt 0 ]]; do
   shift
 done
 
-echo "File name: $filename"
-read -p "cmd:" cmd
-echo "cmd : : $cmd" 
+if [[ "$mode" == "copy" ]]; then
+  rm -r "/tmp/msd/tmp_copy.txt"
+  touch "/tmp/msd/tmp_copy.txt"
+  filename="/tmp/msd/tmp_copy.txt"
+  read -p "cmd:" cmd
+  calculate 
+  formatAndSaveFile $filename
+  cat $filename | xsel -b
+elif [[ "$mode" == "copyDoc" ]]; then
+  if [ -z "$filename" ]; then
+    echo "Please enter a file name using -f from which you want to copy"
+    exit
+  fi
+  cat $filename | xsel -b
+
+else
+  if [ -z "$filename" ]; then
+    echo "Please enter a file name using -f or use copy mode using -c to directly copy to your clipboard"
+    exit
+  fi
+  read -p "cmd:" cmd
+  calculate 
+  formatAndSaveFile $filename
+  
+fi
 
 
 
-pwd=$(pwd)
-hostname=$(hostname)
-whoami=$(whoami)
-$cmd > /tmp/msd/tmp_out.txt
-
-
-echo "==============$title====================" >> $filename
-echo "$whoami@$hostname at $pwd" >> $filename
-echo "" >> $filename
-echo "Command : $cmd" >> $filename
-echo "" >> $filename
-echo "$(cat /tmp/msd/tmp_out.txt)"
-echo "Output : $(cat /tmp/msd/tmp_out.txt)" >> $filename
-echo "========================================" >> $filename
-
-echo "" >> $filename
 
